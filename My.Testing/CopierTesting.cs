0573ee6ee2e5b183ad025e4fe9527e114f293020
Navirter <navirter@gmail.com>
2019-04-19 01:15:54 +0300

    Copier -Removed all functions -Added CopyUsingSerializer<T> -Removed all tests I have no idea how to deep copy these IEnumerables.

--- My.Testing/CopierTesting.cs
+++ My.Testing/CopierTesting.cs
@@ -8,128 +8,129 @@ using System.Threading.Tasks;
 
 namespace My.Testing
 {
-    [TestClass()]
-    public class CopierTesting
-    {
-        class Dude
-        {
-            public string name = "";
-            public int age = 0;
-            public Sex sex = Sex.Male;
-            public enum Sex
-            {
-                Male,
-                Female                
-            }
-            public Dude(string name, int age, Sex sex)
-            {
-                this.name = name;
-                this.age = age;
-                this.sex = sex;                
-            }
-            public Dude() { }
-            public override bool Equals(object obj)
-            {
-                Dude other = obj as Dude;
-                if (name != other.name)
-                    return false;
-                if (age != other.age)
-                    return false;
-                if (sex != other.sex)
-                    return false;
-                return true;
-            }            
-        }
+    //[TestClass()]
+    //public class CopierTesting
+    //{
+    //    class Dude
+    //    {
+    //        public string name = "";
+    //        public int age = 0;
+    //        public Sex sex = Sex.Male;
+    //        public enum Sex
+    //        {
+    //            Male,
+    //            Female                
+    //        }
+    //        public Dude(string name, int age, Sex sex)
+    //        {
+    //            this.name = name;
+    //            this.age = age;
+    //            this.sex = sex;                
+    //        }
+    //        public Dude() { }
+    //        public override bool Equals(object obj)
+    //        {
+    //            Dude other = obj as Dude;
+    //            if (name != other.name)
+    //                return false;
+    //            if (age != other.age)
+    //                return false;
+    //            if (sex != other.sex)
+    //                return false;
+    //            return true;
+    //        }            
+    //    }
 
-        class Parent : Dude
-        {
-            public int kidsAmount = 0;
+    //    class Parent : Dude
+    //    {
+    //        public int kidsAmount = 0;
 
-            public Parent(string name, int age, Sex sex, int kids)
-                : base(name, age, sex)
-            {
-                this.kidsAmount = kids;
-            }
-            public override bool Equals(object obj)
-            {
-                Parent p = obj as Parent;
-                if (!base.Equals(p))
-                    return false;
-                return p.kidsAmount == kidsAmount;
-            }
-        }
+    //        public Parent() { }
+    //        public Parent(string name, int age, Sex sex, int kids)
+    //            : base(name, age, sex)
+    //        {
+    //            this.kidsAmount = kids;
+    //        }
+    //        public override bool Equals(object obj)
+    //        {
+    //            Parent p = obj as Parent;
+    //            if (!base.Equals(p))
+    //                return false;
+    //            return p.kidsAmount == kidsAmount;
+    //        }
+    //    }
 
-        [TestMethod()]
-        public void copyFieldsTesting_SimpleClass()
-        {
-            Dude d = new Dude("Valera", 22, Dude.Sex.Male);
-            Dude res = new Dude();
-            Copier.copyFields(d, res);
-            Assert.AreEqual(d, res);
-        }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_SimpleClass()
+    //    {
+    //        Dude d = new Dude("Valera", 22, Dude.Sex.Male);
+    //        Dude res = new Dude();
+    //        Copier.CopyFields(d, ref res);
+    //        Assert.AreEqual(d, res);
+    //    }
 
-        [TestMethod()]
-        public void copyFieldsTesting_Null()
-        {
-            try
-            {
-                Dude d = null;
-                Dude res = new Dude();
-                Copier.copyFields(d, res);
-                Assert.Fail();
-            }
-            catch
-            { Assert.IsTrue(true); }
-        }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_Null()
+    //    {
+    //        try
+    //        {
+    //            Dude d = null;
+    //            Dude res = new Dude();
+    //            Copier.CopyFields(d, ref res);
+    //            Assert.Fail();
+    //        }
+    //        catch
+    //        { Assert.IsTrue(true); }
+    //    }
 
-        [TestMethod()]
-        public void copyFieldsTesting_Inheritation()
-        {
-            Dude d = new Dude("Valera", 22, Dude.Sex.Male);
-            Parent res = new Parent("Vitaliy", 44, Dude.Sex.Female, 4);//subclass of Dude
-            Copier.copyFields(d, res);
-            bool match = d.name == res.name && d.age == res.age && d.sex == res.sex;
-            Assert.IsTrue(match);
-        }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_Inheritation()
+    //    {
+    //        Dude d = new Dude("Valera", 22, Dude.Sex.Male);
+    //        Parent res = new Parent("Vitaliy", 44, Dude.Sex.Female, 4);//subclass of Dude
+    //        Copier.CopyFields(d, ref res);
+    //        bool match = d.name == res.name && d.age == res.age && d.sex == res.sex;
+    //        Assert.IsTrue(match);
+    //    }
         
-        [TestMethod()]
-        public void copyFieldsTesting_ReverseInheritation()
-        {
-            Parent d = new Parent("Vitaliy", 44, Dude.Sex.Female, 4);//subclass of Dude
-            Dude res = new Dude("Valera", 22, Dude.Sex.Male);
-            Copier.copyFields(d, res);
-            bool match = d.name == res.name && d.age == res.age && d.sex == res.sex;
-            Assert.IsTrue(match);
-        }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_ReverseInheritation()
+    //    {
+    //        Parent d = new Parent("Vitaliy", 44, Dude.Sex.Female, 4);//subclass of Dude
+    //        Dude res = new Dude("Valera", 22, Dude.Sex.Male);
+    //        Copier.CopyFields(d, ref res);
+    //        bool match = d.name == res.name && d.age == res.age && d.sex == res.sex;
+    //        Assert.IsTrue(match);
+    //    }
         
-        [TestMethod()]
-        public void copyFieldsTesting_BrakeReference()
-        {
-            try
-            {
-                Dude d = new Dude();
-                Dude res = new Dude();
-                Copier.copyFields(d, res);
-                d.age = res.age + 1;
-                Assert.AreNotEqual(d.age, res.age);
-            }
-            catch(Exception e)
-            { Assert.Fail(e.Message); }
-        }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_BrakeReference()
+    //    {
+    //        try
+    //        {
+    //            Dude d = new Dude();
+    //            Dude res = new Dude();
+    //            Copier.CopyFields(d, ref res);
+    //            d.age = res.age + 1;
+    //            Assert.AreNotEqual(d.age, res.age);
+    //        }
+    //        catch(Exception e)
+    //        { Assert.Fail(e.Message); }
+    //    }
 
-        [TestMethod()]
-        public void copyFieldsTesting_BrakeReferenceInList()
-        {
-            try
-            {
-                List<Dude> dudes = new List<Dude>() { new Dude() };
-                List<Dude> res = new List<Dude>();
-                Copier.copyFields(dudes, res);
-                dudes[0].age = res[0].age + 1;
-                Assert.AreNotEqual(dudes[0].age, res[0].age);
-            }
-            catch (Exception e)
-            { Assert.Fail(e.Message); }
-        }
-    }
+    //    [TestMethod()]
+    //    public void copyFieldsTesting_BrakeReferenceInList()
+    //    {
+    //        try
+    //        {
+    //            List<Dude> dudes = new List<Dude>() { new Dude() };
+    //            List<Dude> res = new List<Dude>();
+    //            Copier.CopyFields(dudes, ref res);
+    //            dudes[0].age = 2;
+    //            Assert.AreNotEqual(dudes[0].age, res[0].age);
+    //        }
+    //        catch (Exception e)
+    //        { Assert.Fail(e.Message); }
+    //    }
+    //}
 }
--- My/Copier.cs
+++ My/Copier.cs
@@ -4,78 +4,260 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace My
 {
 
     public static class Copier
     {
-
-        class TypeNameComparer : IEqualityComparer<Type>
+        public static T CopyUsingSerializer<T>(T obj)
         {
-            // Products are equal if their names and product numbers are equal.
-            public bool Equals(Type x, Type y)
+            using (var ms = new MemoryStream())
             {
-                //Check whether the compared objects reference the same data.
-                if (Object.ReferenceEquals(x, y)) return true;
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
 
-                //Check whether any of the compared objects is null.
-                if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
-                    return false;
-
-                //Check whether the products' properties are equal.
-                return  x.Name == y.Name;
-            }
-
-            // If Equals() returns true for a pair of objects 
-            // then GetHashCode() must return the same value for these objects.
-
-            public int GetHashCode(Type t)
-            {
-                //Check whether the object is null
-                if (Object.ReferenceEquals(t, null)) return 0;
-
-                //Get hash code for the Name field if it is not null.
-                int hashProductName = t.Name == null ? 0 : t.Name.GetHashCode();
-                
-                return hashProductName;
+                var res = (T)formatter.Deserialize(ms);
+                return res;
             }
         }
-        /// <summary>
-        /// Supports copying fields from a parent class to its inheritants and vice versa. In this case the present in both classes fields are copied only
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="destination"></param>
-        /// <param name="onlyPublic"></param>
-        public static void copyFields<T>(T source, T destination, bool onlyPublic = false)
-        {
-            Type type = source.GetType();
-            Type destinationType = destination.GetType();
-            bool asssignable = destinationType.IsSubclassOf(type) || type.IsSubclassOf(destinationType);
-            if (type != destinationType && !asssignable)
-                throw new InvalidCastException("Types of objects are not the same and can't be inherited right!");
-            var typeFields = new List<FieldInfo>();
-            var destinationTypeFields = new List<FieldInfo>();
-            if (onlyPublic)
-            {
-                typeFields = type.GetFields(BindingFlags.Public | BindingFlags.Instance).ToList();
-                destinationTypeFields = destinationType.GetFields(BindingFlags.Public | BindingFlags.Instance).ToList();
-            }
-            else
-            {
-                typeFields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToList();
-                destinationTypeFields = destinationType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToList();
-            }
 
-            foreach (var sourceField in typeFields)
-            {
-                var match = destinationTypeFields.FirstOrDefault(s => s.Name == sourceField.Name);
-                if (match == null)
-                    continue;
-                var value = match.GetValue(source);
-                match.SetValue(destination, value);
-            }
-        }        
+        //public static Y DeepCopy<T, Y>(T source, bool onlyPublic = false, bool ignoreAssignibility = false) where Y:new()
+        //{
+        //    if (source == null)
+        //        throw new ArgumentNullException("Object cannot be null");
+        //    Type type = source.GetType();
+        //    Y res = new Y();
+        //    Type destinationType = res.GetType();
+        //    if (!ignoreAssignibility)
+        //    {
+        //        bool asssignable = destinationType.IsSubclassOf(type) || type.IsSubclassOf(destinationType);
+        //        if (type != destinationType && !asssignable)
+        //            throw new InvalidCastException("Types of objects are not the same and even not inherinants!");
+        //    }
+        //    var result = (Y)Process(source, destinationType);
+        //    return result;
+        //}
+
+        //static object Process(object source, Type destinationType, bool onlyPublic = false)
+        //{
+        //    if (source == null)
+        //        return null;
+        //    Type type = source.GetType();
+        //    if (type.IsValueType || type == typeof(string))
+        //    {
+        //        return source;
+        //    }
+        //    else if (type.IsArray)
+        //    {
+        //        Type elementType = Type.GetType(
+        //             type.FullName.Replace("[]", string.Empty));
+        //        var array = source as Array;
+        //        Array copied = Array.CreateInstance(elementType, array.Length);
+        //        for (int i = 0; i < array.Length; i++)
+        //        {
+        //            var localValue = array.GetValue(i);
+        //            Type fieldType = localValue.GetType();
+        //            copied.SetValue(Process(localValue, fieldType), i);
+        //        }
+        //        return Convert.ChangeType(copied, source.GetType());
+        //    }
+        //    else if (type.GetInterfaces()
+        //            .FirstOrDefault(i => i.Name == "IEnumerable") != null)
+        //    {                
+        //        Type elementType = Type.GetType(
+        //             type.FullName.Replace("[]", string.Empty));
+        //        dynamic sourceIenum = source;
+        //        for (int i = 0; i < sourceIenum.Count; i++)
+        //        {
+        //            sourceIenum[i] = Process(sourceIenum[i], sourceIenum[i].GetType());
+        //        }
+        //        return sourceIenum;
+        //        //var sourceTypeIenum = sourceIenum.Select(s =>
+        //        //{
+        //        //    var sType = s.GetType();
+        //        //    var res = Convert.ChangeType(s, sType);
+        //        //    return res;
+        //        //}).AsEnumerable();
+        //        //return sourceTypeIenum;
+        //        //var sourceArray = sourceIenum.ToArray();
+        //        //var newArray = new object[sourceArray.Length];
+        //        //for (int i = 0; i < sourceArray.Length; i++)
+        //        //    newArray[i] = Process(sourceArray[i], sourceArray[i].GetType());
+        //        //var list = newArray.Select(s =>
+        //        //{
+        //        //    var sType = s.GetType();
+        //        //    var res = Convert.ChangeType(s, sType);
+        //        //    return res;
+        //        //}).ToList();
+        //        //var final = Convert.ChangeType(list, destinationType);
+        //        //Array copied = Array.CreateInstance(elementType, sourceArray.Length);
+        //        //for (int i = 0; i < copied.Length; i++)
+        //        //    try
+        //        //    {
+        //        //        var localValue = newArray[i];
+        //        //        Type fieldType = localValue.GetType();
+        //        //        copied.SetValue(localValue, i);
+        //        //    }
+        //        //    catch(Exception e)
+        //        //    { }
+        //        //return Convert.ChangeType(copied, destinationType);
+        //    }
+        //    else if (type.IsClass)
+        //    {
+        //        object destinationObject = Activator.CreateInstance(destinationType);
+        //        BindingFlags flags = BindingFlags.Public;
+        //        if (onlyPublic)
+        //            flags = BindingFlags.Public | BindingFlags.Instance;
+        //        else
+        //            flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+        //        FieldInfo[] fields = type.GetFields(flags);
+        //        foreach (FieldInfo field in fields)
+        //            try
+        //            {
+        //                object fieldValue = field.GetValue(source);
+        //                if (fieldValue == null)
+        //                    continue;
+        //                Type fieldType = fieldValue.GetType();
+        //                field.SetValue(destinationObject, Process(fieldValue, fieldType));
+        //            }
+        //            catch { }
+        //        return destinationObject;
+        //    }
+        //    else
+        //        throw new ArgumentException("Unknown type: " + type.FullName);
+        //}
+
+
+
+
+        //[Obsolete("Use DeepCopy")]
+        ///// <summary>
+        ///// Supports copying fields with the same name form one object to another
+        ///// a parent class to its inheritants and vice versa. In this case the present in both classes fields are copied only
+        ///// Both objects must have a parameterless constructor
+        ///// </summary>
+        ///// <param name="source">Object to get fields' values from</param>
+        ///// <param name="destination">Object to set fields' values to if they are present</param>
+        ///// <param name="onlyPublic">Set true to copy only public fields</param>    
+        ///// <param name="ignoreAssignibility">If true, source and destination can be of any classes.\nOtherwise they must be of the same class or one must inherit from another</param>        
+        //public static void CopyFields<T, Y>(T source, ref Y destination, bool onlyPublic = false, bool ignoreAssignibility = false) 
+        //    where Y: new()
+        //{
+        //    destination = CopyUsingSerializer(source);
+        //    //destination = DeepCopy<T, Y>(source, onlyPublic, ignoreAssignibility);
+
+        //    //Type type = source.GetType();
+        //    //Type destinationType = destination.GetType();
+
+        //    //if (!ignoreAssignibility)
+        //    //{
+        //    //    bool asssignable = destinationType.IsSubclassOf(type) || type.IsSubclassOf(destinationType);
+        //    //    if (type != destinationType && !asssignable)
+        //    //        throw new InvalidCastException("Types of objects are not the same and can't be inherited right!");
+        //    //}
+
+        //    //var typeFields = new List<FieldInfo>();
+        //    //var destinationTypeFields = new List<FieldInfo>();
+        //    //if (onlyPublic)
+        //    //{
+        //    //    typeFields = type.GetFields(BindingFlags.Public | BindingFlags.Instance).ToList();
+        //    //    destinationTypeFields = destinationType.GetFields(BindingFlags.Public | BindingFlags.Instance).ToList();
+        //    //}
+        //    //else
+        //    //{
+        //    //    typeFields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToList();
+        //    //    destinationTypeFields = destinationType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToList();
+        //    //}
+            
+
+        //    //if (type.IsValueType || type == typeof(string))
+        //    //{
+        //    //    object thing = source;
+        //    //    object thing2 = destination;
+        //    //    thing2 = thing;
+        //    //    destination = (Y)thing2;
+        //    //}
+        //    //else if (type.IsArray)
+        //    //{
+        //    //    Type elementType = Type.GetType(
+        //    //         type.FullName.Replace("[]", string.Empty));
+        //    //    var array = source as Array;
+        //    //    Array copied = Array.CreateInstance(elementType, array.Length);
+        //    //    for (int i = 0; i < array.Length; i++)
+        //    //    {
+        //    //        copied.SetValue(Process(array.GetValue(i)), i);
+        //    //    }
+        //    //    object thing = Convert.ChangeType(copied, destination.GetType());
+        //    //    destination = (Y)thing;
+        //    //}
+        //    //else if (type.IsClass)
+        //    //{
+        //    //    object toret = Activator.CreateInstance(destination.GetType());
+        //    //    foreach (FieldInfo field in destinationTypeFields)
+        //    //        try
+        //    //        {
+        //    //            object fieldValue = field.GetValue(source);
+        //    //            if (fieldValue == null)
+        //    //                continue;
+        //    //            field.SetValue(toret, Process(fieldValue));
+        //    //        }
+        //    //        catch (ArgumentException ae)
+        //    //        {
+
+        //    //        }
+        //    //        catch(Exception e)
+        //    //        {
+        //    //            throw new Exception(e.Message);
+        //    //        }
+        //    //    destination = (Y)toret;
+        //    //}
+        //    //else
+        //    //    throw new ArgumentException("Unknown type: " + type.FullName);
+
+        //    //Y Ydest = new Y();
+        //    //foreach (var sourceField in typeFields)
+        //    //{
+        //    //    var match = destinationTypeFields.FirstOrDefault(s => s.Name == sourceField.Name);
+        //    //    if (match == null)
+        //    //        continue;
+        //    //    var value = match.GetValue(source);
+        //    //    var valueType = value.GetType();
+        //    //    //if value is some kind if IEnumerable
+        //    //    if (valueType.GetInterfaces()
+        //    //        .FirstOrDefault(i => i.Name == "IEnumerable") != null)
+        //    //    {
+        //    //        if (value is string)// it is IEnumerable as well
+        //    //        {
+        //    //            match.SetValue(Ydest, new string(value.ToString().ToCharArray()));//string.ToString() must work!
+        //    //        }
+        //    //        else
+        //    //        {
+        //    //            var littleCopy = value;
+        //    //            CopyFields(value, ref littleCopy, true);//recursive assignation of objects in IEnumerables
+        //    //            match.SetValue(Ydest, littleCopy);
+        //    //        }
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        //if (valueType.IsValueType)
+        //    //        //    Ydest = (Y)value;
+        //    //        //else
+        //    //            match.SetValue(Ydest, value);
+        //    //    }              
+        //    //}
+        //    //destination = Ydest;
+        //}
+
+        ////public static T CopyFields<T>(T source, bool onlyPublic = false)
+        //{
+        //    List<T> lsource = new List<T>();            
+        //    lsource.Add(source);
+        //    var ldestination = lsource.Select(s => s).ToList();
+        //    return ldestination.First();
+        //}        
     }
 }
